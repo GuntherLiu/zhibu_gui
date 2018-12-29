475d72b71a655f75c68537fd46d8d469774f14b5
liufj8 <liufj8@lenovo.com>
2018-12-29 17:08:21 +0800

    (lenovo) [event] add event in textCtrl

---
+++ login_event.py
@@ -0,0 +1,41 @@
+#encoding=utf-8
+
+class LoginEvent():
+    def __init__(self):
+        self._username = None
+        self._pwd = None
+        self._captcha = None
+
+        print "login event"
+
+
+    def get_login_msg(self, event):
+        print "get login msg"
+
+        # EventObject 这个对象，就是 组件对象的封装， 内部封装了组件的所有信息
+        event_obj = event.EventObject
+        event_obj.SetValue("")
+
+
+        # print value
+
+        id = event_obj.Id
+
+        # username
+        if id == 1:
+            self._username = event_obj.Value
+
+        # pwd
+        elif id == 2:
+            self._pwd = event_obj.Value
+
+        # captcha
+        else:
+            self._captcha = event_obj.Value
+
+
+
+
+
+
+
--- main_gui.py
+++ main_gui.py
@@ -1,25 +1,67 @@
 #encoding=utf-8
-from wx import App,Frame,Panel
-
+from wx import App,Frame,Panel, Button, Font, GridSizer, TextCtrl, Image, StaticBitmap
+from wx import ROMAN, NORMAL, BITMAP_TYPE_PNG, EVT_LEFT_DOWN, EVT_CHAR, EVT_TEXT
+from login_event import LoginEvent
 
 class Main_gui(App):
     def __init__(self):
+        self._frame = None
+        self._panel = None
+        self._button_login = None
+        self._input_username = None
+        self._input_pwd = None
+        self._input_captcha = None
+        self._panel_login = None
+        self._login_event = LoginEvent()
+
         App.__init__(self)
         print "Main GUI"
 
 
     def OnInit(self):
         print "overwrite on init"
+        self._frame = Frame(parent=None, title="支部工作，一键刷单", pos=(150, 50), size=(1000, 600))
+        # self.Frame.SetMaxSize((1000, 800))
+        self.SetTopWindow(self._frame)
+        self._panel = Panel(self._frame, -1)
+        self._panel_login = Panel(self._panel, -1, pos=(100, 20), size=(420, 340))
+        self.login_button()
+        self.login_input()
+        self.login_captcha()
+        # self.sizer()
 
-        self.Frame = Frame(parent=None, title="支部工作，一键刷单", pos=(150, 50), size=(1000, 600))
-        self.Frame.SetMaxSize((1000, 800))
-        self.SetTopWindow(self.Frame)
-        self.panel = Panel(self.Frame, -1)
 
-        self.Frame.Show()
+        self._frame.Show()
         return True
 
-    
+    def sizer(self):
+        sizer = GridSizer(rows=4, cols=4, hgap=5, vgap=5)
+        sizer.Add(self._button_login)
+        sizer.Add(self._input_username)
+        sizer.Add(self._input_pwd)
+        sizer.Add(self._input_capcha)
+
+    def login_button(self):
+        print "login button"
+
+        self._button_login = Button(self._panel_login, -1, "登录", pos=(100, 150), size=(80, 30))
+        font = Font(18, ROMAN, NORMAL, NORMAL)
+        self._button_login.SetFont(font)
+
+    def login_input(self):
+        print "login msg"
+        self._input_username = TextCtrl(self._panel_login, 1,  pos=(100, 20), size=(200, 40))
+        self._input_username.SetLabel("请输入用户名：")
+        self._input_pwd = TextCtrl(self._panel_login, 2, "请输入密码：", pos=(100,60), size=(200, 40))
+        self._input_captcha = TextCtrl(self._panel_login, 3,  "请输入验证码：", pos=(100, 100), size=(200, 40))
+        self._input_username.Bind(EVT_LEFT_DOWN, self._login_event.get_login_msg)
+
+    def login_captcha(self):
+        print "login captcha"
+        captcha_image = Image('captcha.png', BITMAP_TYPE_PNG).ConvertToBitmap()
+        StaticBitmap(self._panel_login, bitmap=captcha_image, pos=(305, 105))
+
+
 
 
 if __name__ == '__main__':
